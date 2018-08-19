215f4826a73264a9689807830f8375342632c80f
TheBlocks <21197843+TheBlocks@users.noreply.github.com>
2018-08-19 20:01:44 +0600

    Add docstring and type hints to PhysicsObject

--- DropBot/objects/physics_object.py
+++ DropBot/objects/physics_object.py
@@ -3,8 +3,12 @@ from ..bot_math.Vector3 import Vector3
 
 
 class PhysicsObject:
+    """Class used as an abstraction layer over `game_data_struct.Physics`.
+    Used for objects that have physics components, i.e. cars and the ball.
+    It has the same class variables as `game_data_struct.Physics`, but with our `Vector3` class instead."""
+
     def __init__(self, physics: game_data_struct.Physics):
-        self.location = Vector3(physics.location)
-        self.velocity = Vector3(physics.velocity)
-        self.rotation = Vector3(physics.rotation)
-        self.angular_velocity = Vector3(physics.angular_velocity)
+        self.location: Vector3 = Vector3(physics.location)
+        self.velocity: Vector3 = Vector3(physics.velocity)
+        self.rotation: Vector3 = Vector3(physics.rotation)
+        self.angular_velocity: Vector3 = Vector3(physics.angular_velocity)
