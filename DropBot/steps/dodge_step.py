b4ccc58ceaa5b02aa574ffe1c87343b3ac3e9de6
TheBlocks <21197843+TheBlocks@users.noreply.github.com>
2018-11-12 21:20:54 +0100

    Add DodgeStep

---
+++ DropBot/steps/dodge_step.py
@@ -0,0 +1,53 @@
+from rlbot.utils.structures.game_data_struct import GameTickPacket, FieldInfoPacket
+from rlbot.agents.base_agent import SimpleControllerState
+from .base_step import BaseStep
+from DropBot.bot_math.Vector3 import Vector3
+from DropBot.objects.physics_object import PhysicsObject
+from typing import Optional
+import math
+
+
+class DodgeStep(BaseStep):
+    def __init__(self, name: str, team: int, index: int, field_info: FieldInfoPacket, target: Vector3, dodge_time: float = 0.2):
+        super().__init__(name, team, index, field_info)
+        self.target: Vector3 = target
+
+        self.on_second_jump = False
+        self.next_dodge_time = 0
+        self.dodge_time = dodge_time
+
+    def get_output(self, packet: GameTickPacket) -> Optional[SimpleControllerState]:
+        controller = SimpleControllerState()
+        bot = PhysicsObject(packet.game_cars[self.index].physics)
+
+        if packet.game_info.seconds_elapsed > self.next_dodge_time:
+            controller.jump = True
+
+            # Calculate pitch and roll based on target and bot position
+
+            # Correct yaw from (0 to pi to -pi to 0), to (0 to 2pi).
+            # Then rotate circle by pi/2 degrees. Then flip circle vertically.
+            yaw = bot.rotation.z
+            if yaw < 0:
+                yaw += 2 * math.pi
+            yaw -= math.pi / 2
+            if yaw < 0:
+                yaw += 2 * math.pi
+            yaw = 2 * math.pi - yaw
+
+            direction_to_target = (self.target - bot.location).normalised()
+            angle_to_target = math.atan2(direction_to_target.y, direction_to_target.x)
+            angle = angle_to_target - yaw
+
+            controller.pitch = -math.cos(angle)
+            controller.roll = math.sin(angle)
+
+            if self.on_second_jump:
+                return None
+            else:
+                self.on_second_jump = True
+                self.next_dodge_time = packet.game_info.seconds_elapsed + self.dodge_time
+        else:
+            controller.jump = False
+
+        return controller
